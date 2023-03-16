9cd4d4de8cbf51daa8501b9e6d9c40860b301527
tahaeddiani <tahaeddiani2@gmail.com>
2023-03-16 10:11:28 -0700

    fast commit charak

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - beginning
+*
+* Return: Always 0 (Valid)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
