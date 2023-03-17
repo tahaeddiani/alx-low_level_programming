ad5749e84541eb3d83d69004f4c37fadd0794274
tahaeddiani <tahaeddiani2@gmail.com>
2023-03-17 08:33:57 -0700

    fast commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main (void)
 {
-	char lowcase;
-
-	for (lowcase = 'a'; lowcase <= 'z'; lowcase++){
-		putchar(lowcase);
-		putchar('\n');
-		}
-	return (0);
+char lowcase;
+for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
+{
+putchar(lowcase);
+}
+return (0);
 }
