789e143d1a664dac73d651992af1b73fd1153b69
tahaeddiani <tahaeddiani2@gmail.com>
2023-03-17 07:50:40 -0700

    fast commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
+/*main: beginnig 
+ /*betty style doc for function main goes there */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
