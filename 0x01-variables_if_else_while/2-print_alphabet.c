2bc51be9595895267f5abcbedd936ba7b6ae13e9
tahaeddiani <tahaeddiani2@gmail.com>
2023-03-17 08:24:52 -0700

    fast commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowcase;
+
+	for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
+		putchar(lowcase);
+		putchar('\n');
+
+	return (0);
+}
