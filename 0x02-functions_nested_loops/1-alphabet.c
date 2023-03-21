65d0b3cfb4e1c92779735f42f8326b9439693500
tahaeddiani <tahaeddiani2@gmail.com>
2023-03-21 12:39:57 -0700

    super commit charak

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,20 @@
 #include "main.h"
-/**
- *main - Entry point
- *
- *Description: prints the alphabet in lowercase followed by new line
- *
- *Return: Always 0 (Success)
- */
-void print_alphabet(void)
-{
-	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
-}
+
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
+
