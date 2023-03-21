71c8c528b3c14d86c7e4ec7726b4bc51e0b8866a
tahaeddiani <tahaeddiani2@gmail.com>
2023-03-21 12:44:09 -0700

    super commit charak

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
 #include "main.h"
-
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
+/**
+* main
+* print_alphabet - print all alphabet in lowercase
+* description: It prints the alphabet in lowercase fallowed by a new line
+* return 0: succes
+* 
+*/
 
 
 	void print_alphabet(void)
