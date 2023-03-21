12996d934e2da2529e6efd9cc0b18f5fa4cf1dd6
tahaeddiani <tahaeddiani2@gmail.com>
2023-03-21 13:29:24 -0700

    super commit charak

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,22 @@
 #include "main.h"
+
 /**
- * print_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from
- * Return: value of the last digit
+ * jack_bauer - prints every minute of the day of Jack Bauer
  */
-int print_last_digit(int n)
+void jack_bauer(void)
 {
-int a;
-if (n < 0)
-n = -n;
-a = n % 10;
-if (a < 0)
-a = -a;
-_putchar(a + '0');
-return (a);
+	int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+		_putchar((hours / 10) + '0');
+		_putchar((hours % 10) + '0');
+		_putchar(':');
+		_putchar((minutes / 10) + '0');
+		_putchar((minutes % 10) + '0');
+		_putchar('\n');
+		}
+	}
 }
