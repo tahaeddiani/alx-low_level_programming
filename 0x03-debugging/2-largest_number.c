d374f733027ababc124a38bb7ab0d94868d7d151
tahaeddiani <tahaeddiani2@gmail.com>
2023-03-22 08:34:24 -0700

    money loves speed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,6 +7,7 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -15,14 +16,15 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
+
 	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	else if (c >= a && c >= b)
+
+	else
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
