e033aafa8ab9d2e66e567674a9e00673b56d7c2e
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-06 15:44:22 -0700

    fast commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 /**
  * _memcpy - function copies the memory area
  *
- * @dest: buffer where we will copy 
+ * @dest: buffer where we will copy
  * @src: what we are to copy.
  * @n: n bytes of @src.
  *
