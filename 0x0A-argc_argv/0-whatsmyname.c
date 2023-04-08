573edfe7bc1e295db05f9f1cfff64271d99e14d5
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-08 15:43:55 -0700

    fast commit v7

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,18 @@
 /**
  * main - main function where the main code is
  *
- *
+ *@argc: an int variable
+ *@argv: variable type char
+ *Return: return when success
  */
-int main(int argc, char** argv){
+
+int main(int argc, char **argv)
+{
 	int loop;
+
 	for (loop = 0; loop < argc; loop++)
 {
-		printf("%s",argv[loop]);
+		printf("%s", argv[loop]);
 }
+	return (0);
 }
