05fdc9cda14dea7ffea766d2627414ba17d4d093
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-09 07:44:53 -0700

    fast commit v7

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: variable type char.
  * Return: return 0 when success.
  */
-int main(int argc, char *argv[void])
+int main(int argc, char void **argv)
 {
 	printf("%i",argc - 1);
 	return (0);
