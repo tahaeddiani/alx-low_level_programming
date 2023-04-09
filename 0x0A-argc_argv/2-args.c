798d9f4783472350cb18271963d77167a05cfc32
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-09 07:49:13 -0700

    fast commit v7

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - where the main code is.
+ * @argc: vriable type intiger
+ * @argv: variable type char.
+ * Return: return 0 when success.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+{
+	printf("%s\n", argv[i]);
+}
+	return (0);
+}
