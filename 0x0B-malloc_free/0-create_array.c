011d0def4de113412122966415728f841d8544c5
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-11 10:13:50 -0700

    0x0B. C - malloc, free²

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,26 +4,7 @@
 /**
  *
  * main - the main function
- *
+ * @c: type charater
+ * @size: type int
  * Return: it return 0 if success
  */
-
-char *create_array(unsigned int size, char c)
-{
-
-char *ptr;
-*ptr = (char *) malloc (size * sizeof(char) + 1);
-*ptr = c;
-
-if (size == 0 || ptr == NULL)
-
-{
-return NULL;
-}
-
-else
-{
-return (ptr);
-}
-
-}
