f2af5d08e061fdce49a53baf2607b53bb61ba379
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-12 08:49:17 -0700

    t2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - function that concatenates
+ * @s1: string1
+ * @s2: string2
+ * Return: z
+ */
+char *str_concat(char *s1, char *s2)
+	{
+	int x, f, c, t;
+	char *o;
+
+	if (s1 == NULL)
+	{
+	s1 = "";
+	}
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+	}
+	if (s2 == NULL)
+	{
+	s2 = "";
+	}
+	for (f = 0; s2[f] != '\0'; f++)
+	{
+	}
+	o = malloc(sizeof(char) * (x + f + 1));
+	if (o == NULL)
+	{
+	return (NULL);
+	}
+	for (c = 0; c < x; c++)
+	{
+	o[c] = s1[c];
+	}
+	for (t = 0; t < f; t++)
+	{
+	o[c] = s2[t];
+	c++;
+	}
+
+	return (o);
+	}
