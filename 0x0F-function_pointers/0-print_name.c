c67d25d92f4bae7638a352e4cce7c88c3778e12e
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-19 11:43:54 -0700

    t0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ * @f: printing function pointer
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *));
+	if (name && f)
+	f(name);
+	return (0);
