67781150718747982f7ff8ff99edc6a48d21e5d3
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-25 10:09:32 -0700

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,21 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef dog_H
+#define dog_H
 /**
- * struct dog - dog
- * @name: first element
- * @age: second element
- * @owner: third element
+ * struct dog - Define a new type vars
+ * @name: name char dog
+ * @age: age float dog
+ * @owner: owner char dog
  */
+
 struct dog
 {
-char *name;
-float age;
-char *owner;
+char  *name;
+float  age;
+char  *owner;
 };
-/**
- * dog_t - type
- */
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *neew_dog(char *name, float age, char *owner);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 #endif
