9ede0dfaf9bfc29c7e8ec41ecffdfc0a5da48f20
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-25 10:01:50 -0700

    fast commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+/**
+ * dog_t - type
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *neew_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+#endif
