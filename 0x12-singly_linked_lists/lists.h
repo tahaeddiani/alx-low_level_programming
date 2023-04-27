779cfdfc449199b971869e3bdf4acda26dc6ce0f
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-27 14:38:21 -0700

    task0

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,33 @@
+#ifndef LIST_H
+#define LIST_H
+
+/**
+ * struct list_s - linked list
+ * @str: string - malloc string
+ * @len: length of rhe string.
+ * @next: points to the next node
+ */
+#ifndef LIST_H
+#define LIST_H
+
+/**
+ * struct list_s - linked list
+ * @str: string - malloc string
+ * @len: length of rhe string.
+ * @next: points to the next node
+ */
+
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
