8276db81eea5672a52d9b3884a459e795217d310
tahaeddiani <tahaeddiani2@gmail.com>
2023-06-19 15:35:12 -0700

    fast push 3

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -42,3 +42,21 @@ int div(int a, int b)
 {
 return (a / b);
 }
+/**
+ * mod - Divides two integers.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The quotient of the division (a / b).
+ */
+int mod(int a, int b)
+{
+if (b != 0)
+{
+return (a % b);
+}
+else
+{
+return (0);
+}
+}
